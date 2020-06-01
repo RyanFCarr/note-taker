8324152ac22b36db43b59049f6fe80b03bd2bbb5
Ryan Carr <ryanfcarr@gmail.com>
2020-06-01 19:40:15 -0400

    Added Ids to the data

--- public/server.js
+++ public/server.js
@@ -1,25 +1,55 @@
-var express = require('express');
-var app = express();
 var path = require('path');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
+const express = require('express');
+const app = express();
+const dbPath = "C:\\Development\\Bootcamp\\GitHub Repo\\note-taker\\db\\db.json";
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.listen(5500);
-console.log("connected on 5500");
+    console.log("connected on 5500");
 
-// viewed at http://localhost:8080
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'))
-});
-
+// GET
 app.get('/notes', function(req, res) {
     res.sendFile(path.join(__dirname + '/notes.html'))
 });
 app.get('/api/notes', function(req, res) {
-    res.sendFile(`C:\\Development\\Bootcamp\\GitHub Repo\\note-taker\\db\\db.json`)
+    res.sendFile(dbPath);
 });
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'))
 });
 
+// POST
+app.post('/', function (req, res) {
+    res.send('POST request to the homepage')
+})
+app.post('/api/notes', function (req, res) {
+    fs.readFile(dbPath, "utf8", (error, data) => {
+        if(error){
+            console.log('error');
+        }else{
+            const db = JSON.parse(data);
+            // Find the greatest value
+            if(!req.body.id){
+                let currentID = db.reduce((accumulator, currentValue)=>{
+                    if(accumulator.id > currentValue.id){
+                        return accumulator.id;
+                    }
+                    return currentValue.id;
+                });
+                req.body.id = currentID +1;    
+            }
+            db.push(req.body);
+            fs.writeFile(dbPath, JSON.stringify(db), ()=>{res.sendStatus(201)})
+        }
+    })
+})
+app.delete('/api/notes/:id', function (req, res) {
+    res.send('Got a DELETE request at /api/notes/:id')
+  
+})
 
